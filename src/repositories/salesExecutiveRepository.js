0942ec0a95bc09d6df1dfa9449c3df907bafc3d8
harpreet singh <harpreet78730@gmail.com>
2024-07-26 01:31:14 +0530

    sales-commission-functionality-integrated

--- src/repositories/salesExecutiveRepository.js
+++ src/repositories/salesExecutiveRepository.js
@@ -1,5 +1,22 @@
 import { AppDataSource } from '../config/data-source.js';
 import { TblSalesExecutives } from '../entities/TblSalesExecutives.js';
+import { Shopkeeper } from '../entities/Shopkeeper.js';
+import { Commission } from '../entities/Commission.js';
+import { TblCommission } from '../entities/TblCommission.js';
+export const getShopkeeperRepo = async () => {
+    return AppDataSource.getRepository(Shopkeeper);
+}
+export const getCommissionRepo = async () => {
+    return AppDataSource.getRepository(Commission);
+}
+export const getTblCommissionRepo = async () => {
+    return AppDataSource.getRepository(TblCommission);
+}
+
+export const getSalesExecutiveRepos = async () => {
+	return AppDataSource.getRepository(TblSalesExecutives);
+}
+
 
 export const checkUserRepository = async (mobileNumber) => {
   try {
--- src/routes/v1/salesExecutiveRoutes.js
+++ src/routes/v1/salesExecutiveRoutes.js
@@ -1,11 +1,14 @@
 // src/routes/v1/salesExecutiveRoutes.js
 
 import { Router } from 'express';
-import { checkUserController ,submitFormController } from '../../controllers/salesExecutiveController.js';
+import { checkUserController ,registerSalesController,submitFormController } from '../../controllers/salesExecutiveController.js';
 
 const router = Router();
 
 router.post('/check-user', checkUserController);  // Route to check if user exists
 router.post('/submit-form', submitFormController); // Route to submit the form
+router.post('/registerSales', registerSalesController); // Route to submit the form
+//Register Shopkeeper from sales executive module 
+
 
 export default router;
